56219fc2d0dcbed68313b9b6dba79ee95ca51733
Ahaochan <844394093@qq.com>
2019-06-27 15:31:15 +0800

    RedisHelper 添加 hset 和 hget

---
+++ src/main/java/com/ahao/util/commons/lang/BeanHelper.java
@@ -0,0 +1,73 @@
+package com.ahao.util.commons.lang;
+
+import org.apache.commons.beanutils.BeanMap;
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+
+public class BeanHelper {
+    private static final Logger logger = LoggerFactory.getLogger(BeanHelper.class);
+
+    @SuppressWarnings("unchecked")
+    public static <T> T cast(Object obj, Class<T> clazz) {
+        // 1. 处理 void
+        if(obj == null || clazz == null || clazz == void.class || clazz == Void.class) {
+            return null;
+        }
+
+        // 2. 从 Redis 中获取数据
+        String string = String.valueOf(obj);
+
+        // 3. 处理 char 类型
+        if(clazz == char.class || clazz == Character.class) {
+            return (T) Character.valueOf(string.charAt(0));
+        }
+
+        // 4. 处理数值类型
+        if(NumberUtils.isCreatable(string)) {
+            if(clazz == byte.class || clazz == Byte.class) {
+                return (T) Byte.valueOf(string);
+            }
+            if(clazz == short.class || clazz == Short.class) {
+                return (T) Short.valueOf(string);
+            }
+            if(clazz == int.class || clazz == Integer.class) {
+                return (T) Integer.valueOf(string);
+            }
+            if(clazz == long.class || clazz == Long.class) {
+                return (T) Long.valueOf(string);
+            }
+            if(clazz == float.class || clazz == Float.class) {
+                return (T) Float.valueOf(string);
+            }
+            if(clazz == double.class || clazz == Double.class) {
+                return (T) Double.valueOf(string);
+            }
+        }
+        return (T) obj;
+    }
+
+    public static Map toMap(Object obj) {
+        return new BeanMap(obj);
+    }
+
+    public static <T> T toBean(Map map, Class<T> clazz) {
+        if(clazz == null || MapUtils.isEmpty(map)) {
+            return null;
+        }
+
+        T obj = ReflectHelper.create(clazz);
+        try {
+            BeanUtils.populate(obj, map);
+            return obj;
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            logger.error("转为Bean失败", e);
+        }
+        return null;
+    }
+}
--- src/main/java/com/ahao/util/spring/redis/RedisHelper.java
+++ src/main/java/com/ahao/util/spring/redis/RedisHelper.java
@@ -1,10 +1,13 @@
 package com.ahao.util.spring.redis;
 
+import com.ahao.util.commons.lang.BeanHelper;
 import com.ahao.util.spring.SpringContextHolder;
-import org.apache.commons.lang3.math.NumberUtils;
+import org.apache.commons.beanutils.BeanMap;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.redis.core.RedisTemplate;
 
 import java.util.Arrays;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class RedisHelper {
@@ -29,42 +32,14 @@ public class RedisHelper {
 
     @SuppressWarnings("unchecked")
     public static <T> T get(String key, Class<T> clazz) {
-        // 1. 处理 void
-        if(clazz == void.class || clazz == Void.class) {
+        // 1. 参数校验
+        if(StringUtils.isEmpty(key) || clazz == null) {
             return null;
         }
 
         // 2. 从 Redis 中获取数据
         Object obj = getRedisTemplate().opsForValue().get(key);
-        String string = String.valueOf(obj);
-
-        // 3. 处理 char 类型
-        if(clazz == char.class || clazz == Character.class) {
-            return (T) Character.valueOf(string.charAt(0));
-        }
-
-        // 4. 处理数值类型
-        if(NumberUtils.isCreatable(string)) {
-            if(clazz == byte.class || clazz == Byte.class) {
-                return (T) Byte.valueOf(string);
-            }
-            if(clazz == short.class || clazz == Short.class) {
-                return (T) Short.valueOf(string);
-            }
-            if(clazz == int.class || clazz == Integer.class) {
-                return (T) Integer.valueOf(string);
-            }
-            if(clazz == long.class || clazz == Long.class) {
-                return (T) Long.valueOf(string);
-            }
-            if(clazz == float.class || clazz == Float.class) {
-                return (T) Float.valueOf(string);
-            }
-            if(clazz == double.class || clazz == Double.class) {
-                return (T) Double.valueOf(string);
-            }
-        }
-        return (T) obj;
+        return BeanHelper.cast(obj, clazz);
     }
     public static int getInt(String key) {
         Object value = getRedisTemplate().opsForValue().get(key);
@@ -91,4 +66,27 @@ public class RedisHelper {
     public static Long decr(String key) {
         return getRedisTemplate().opsForValue().decrement(key);
     }
+
+
+    // ======================================== hash ==================================================
+    public static <T> T hget(String key, Class<T> clazz) {
+        Map entries = getRedisTemplate().boundHashOps(key).entries();
+        return BeanHelper.toBean(entries, clazz);
+    }
+    public static <T> T hget(String key, Object field, Class<T> clazz) {
+        Object value = getRedisTemplate().boundHashOps(key).get(field);
+        return BeanHelper.cast(value, clazz);
+    }
+
+    public static void hset(String key, Object field, Object value) {
+        getRedisTemplate().boundHashOps(key).put(field, value);
+    }
+    public static void hset(String key, Object hash) {
+        hset(key, new BeanMap(hash));
+    }
+    public static void hset(String key, Map hash) {
+        getRedisTemplate().opsForHash().putAll(key, hash);
+    }
+
+    // ======================================== hash ==================================================
 }
