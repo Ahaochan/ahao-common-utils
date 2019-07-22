e3fb5c2a2f4b33f6d13bc28636e95c3536a415d4
Ahaochan <844394093@qq.com>
2019-07-22 16:00:23 +0800

    添加 ObjectHelper

---
+++ src/test/java/com/ahao/util/commons/lang/ObjectHelperTest.java
@@ -0,0 +1,15 @@
+package com.ahao.util.commons.lang;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ObjectHelperTest {
+    @Test
+    public void equalsAny() {
+        Assertions.assertTrue(ObjectHelper.equalsAny("a", "b", "a", "c"));
+        Assertions.assertFalse(ObjectHelper.equalsAny("a", "b", "b", "c"));
+
+        Assertions.assertFalse(ObjectHelper.notEqualsAny("a", "b", "a", "c"));
+        Assertions.assertTrue(ObjectHelper.notEqualsAny("a", "b", "b", "c"));
+    }
+}
