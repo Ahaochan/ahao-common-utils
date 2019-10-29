9cd6b736e94f15392cd126ea5e0010e099593cb9
Ahaochan <844394093@qq.com>
2019-10-29 18:38:32 +0800

    JSONHelper 添加解析集合的方法

--- src/main/java/com/ahao/util/commons/io/JSONHelper.java
+++ src/main/java/com/ahao/util/commons/io/JSONHelper.java
@@ -3,12 +3,18 @@ package com.ahao.util.commons.io;
 
 import com.ahao.util.spring.SpringContextHolder;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.jackson.JacksonProperties;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 public class JSONHelper {
     private static final Logger logger = LoggerFactory.getLogger(JSONHelper.class);
@@ -16,13 +22,12 @@ public class JSONHelper {
     private static ObjectMapper om;
 
     static {
-        if(SpringContextHolder.enable()) {
+        if (SpringContextHolder.enable()) {
             om = SpringContextHolder.getBean(ObjectMapper.class, null);
         }
-        if(om == null) {
+        if (om == null) {
             om = new ObjectMapper();
             JacksonProperties jacksonProperties = new JacksonProperties();
-
         }
     }
 
@@ -54,5 +59,63 @@ public class JSONHelper {
             logger.error(json + " 转为 " + clazz.getName() + " 错误.", e);
         }
         return null;
+    }
+
+    public static <T> List<T> parseList(String json, Class<T> clazz) {
+        JavaType type = om.getTypeFactory().constructParametricType(List.class, clazz);
+        try {
+            List<T> list = om.readValue(json, type);
+            return list;
+        } catch (IOException e) {
+            logger.error(json + " 转为 List<" + clazz.getName() + ">错误.", e);
+        }
+        return Collections.emptyList();
+    }
+
+    public static <K, V> Map<K, V> parseMap(String json, Class<K> keyClass, Class<V> valueClass) {
+        JavaType type = om.getTypeFactory().constructParametricType(Map.class, keyClass, valueClass);
+        try {
+            Map<K, V> map = om.readValue(json, type);
+            return map;
+        } catch (IOException e) {
+            logger.error(json + " 转为 Map<" + keyClass.getName() + ", " + valueClass.getName() + ">错误.", e);
+        }
+        return Collections.emptyMap();
+    }
+
+    public static String getString(String json, String key) {
+        if (StringUtils.isAnyBlank(json, key)) {
+            return "";
+        }
+        try {
+            JsonNode node = om.readTree(json);
+            String[] searchNodes = StringUtils.split(key, '.');
+
+            for (String searchNode : searchNodes) {
+                if (StringUtils.isNumeric(searchNode)) {
+                    node = node.get(Integer.parseInt(searchNode));
+                } else {
+                    node = node.get(searchNode);
+                }
+                if (node == null) {
+                    return "";
+                }
+            }
+            String value = node.asText();
+            return value;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public static int getInt(String json, String key) {
+        String value = getString(json, key);
+        return StringUtils.isNumeric(value) ? Integer.parseInt(value) : 0;
+    }
+
+    public static boolean getBoolean(String json, String key) {
+        String value = getString(json, key);
+        return Boolean.parseBoolean(value);
     }
 }
--- src/test/java/com/ahao/util/commons/io/JSONHelperTest.java
+++ src/test/java/com/ahao/util/commons/io/JSONHelperTest.java
@@ -1,15 +1,16 @@
 package com.ahao.util.commons.io;
 
 import com.ahao.domain.entity.AjaxDTO;
+import com.ahao.domain.entity.BaseDO;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
+import java.util.*;
 
 public class JSONHelperTest {
     @Test
-    public void test() {
+    public void parse() {
         AjaxDTO entity1 = AjaxDTO.get(1, "hello", Arrays.asList(1, 2, 3));
 
         String json1 = JSONHelper.toString(entity1);
@@ -29,5 +30,72 @@ public class JSONHelperTest {
         Assertions.assertEquals(entity1.getResult(), entity2.getResult());
         Assertions.assertEquals(entity1.getMsg(), entity2.getMsg());
         Assertions.assertEquals(entity1.getObj(), entity2.getObj());
+    }
+
+    @Test
+    public void parseList() {
+        List<BaseDO> list1 = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            BaseDO entity = new BaseDO();
+            entity.setId((long) i);
+            entity.setCreateTime(new Date());
+            entity.setUpdateTime(new Date());
+            list1.add(entity);
+        }
+
+        String json = JSONHelper.toString(list1);
+        System.out.println(json);
+        Assertions.assertNotNull(json);
+        Assertions.assertNotEquals("", json);
+
+        List<BaseDO> list2 = JSONHelper.parseList(json, BaseDO.class);
+        Assertions.assertNotNull(list2);
+        Assertions.assertFalse(list2.isEmpty());
+        Assertions.assertEquals(list1, list2);
+    }
+
+    @Test
+    public void parseMap() {
+        HashMap<Integer, BaseDO> map1 = new HashMap<>();
+        for (int i = 0; i < 10; i++) {
+            BaseDO entity = new BaseDO();
+            entity.setId((long) i);
+            entity.setCreateTime(new Date());
+            entity.setUpdateTime(new Date());
+            map1.put(i, entity);
+        }
+
+        String json = JSONHelper.toString(map1);
+        System.out.println(json);
+        Assertions.assertNotNull(json);
+        Assertions.assertNotEquals("", json);
+
+        Map<Integer, BaseDO> map2 = JSONHelper.parseMap(json, Integer.class, BaseDO.class);
+        Assertions.assertNotNull(map2);
+        Assertions.assertFalse(map2.isEmpty());
+        Assertions.assertEquals(map1, map2);
+    }
+
+    @Test
+    public void getNode() {
+        // {"status1":1,"message1":"message1","deep1":{"status2":2,"message2":"message2","deep2":[1,2]}}
+        String json = "{\"status1\":1,\"message1\":\"message1\",\"deep1\":{\"status2\":2,\"message2\":\"message2\",\"deep2\":[1,2]}}";
+        System.out.println(json);
+
+        String message1 = JSONHelper.getString(json, "message1");
+        String message2 = JSONHelper.getString(json, "deep1.message2");
+        String message3 = JSONHelper.getString(json, "deep1.message3");
+        Assertions.assertEquals("message1", message1);
+        Assertions.assertEquals("message2", message2);
+        Assertions.assertEquals("", message3);
+
+        String data1 = JSONHelper.getString(json, "deep1.deep2.0");
+        String data2 = JSONHelper.getString(json, "deep1.deep2.1");
+        Assertions.assertEquals("1", data1);
+        Assertions.assertEquals("2", data2);
+        int int1 = JSONHelper.getInt(json, "deep1.deep2.0");
+        int int2 = JSONHelper.getInt(json, "deep1.deep2.1");
+        Assertions.assertEquals(1, int1);
+        Assertions.assertEquals(2, int2);
     }
 }
