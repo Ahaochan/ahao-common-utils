061c5b05deec189d304f847a95860a6852c2385e
Ahaochan <844394093@qq.com>
2019-11-13 11:58:35 +0800

    MailHelper 重构优化

--- src/main/java/com/ahao/util/spring/mail/MailHelper.java
+++ src/main/java/com/ahao/util/spring/mail/MailHelper.java
@@ -1,17 +1,21 @@
 package com.ahao.util.spring.mail;
 
 import com.ahao.util.spring.SpringContextHolder;
+import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.mail.MailProperties;
-import org.springframework.core.io.InputStreamSource;
+import org.springframework.core.io.Resource;
 import org.springframework.mail.MailException;
+import org.springframework.mail.MailSendException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
-import java.util.Map;
+import java.util.*;
 
 public class MailHelper {
     private static final Logger logger = LoggerFactory.getLogger(MailHelper.class);
@@ -47,44 +51,185 @@ public class MailHelper {
     }
     // ======================================== 依赖 ==================================================
 
-    public static boolean sendText(String email, String title, String content) {
-        return sendTextWithFile(email, title, content, null);
+    public static Builder setSubject(String subject) {
+        Builder builder = new Builder();
+        builder.subject = subject;
+        return builder;
     }
 
-    public static boolean sendTextWithFile(String email, String title, String content, Map<String, InputStreamSource> files) {
-        return send(email, title, content, false, files);
-    }
+    public static class Builder {
+        private String subject;
+        private String content;
+        private boolean html;
+        private String replyEmail;
+        private Set<String> toEmails = new HashSet<>();
+        private Set<String> ccEmails = new HashSet<>();
+        private Set<String> bccEmails = new HashSet<>();
+        private Map<String, Resource> inlineFile;
+        private Map<String, Resource> extendFile;
 
-    public static boolean sendHtml(String email, String title, String html) {
-        return sendHtmlWithFile(email, title, html, null);
-    }
+        private MailPriority priority = MailPriority.NORMAL;
+        private Date sendDate;
 
-    public static boolean sendHtmlWithFile(String email, String title, String html, Map<String, InputStreamSource> files) {
-        return send(email, title, html, true, files);
-    }
+        public void send() {
+            logger.debug("发送邮件标题:{}, 内容:{}", subject, content);
+            logger.debug("邮件收件人: {}", toEmails);
+            logger.debug("邮件抄送: {}", ccEmails);
+            logger.debug("邮件密抄: {}", bccEmails);
+            try {
+                MimeMessage mimeMessage = getMailSender().createMimeMessage();
 
-    public static boolean send(String email, String title, String content, boolean html, Map<String, InputStreamSource> files) {
-        logger.debug("发送邮件给{}, 标题:{}, 内容:{}", email, title, content);
-        try {
-            MimeMessage mimeMessage = getMailSender().createMimeMessage();
-
-            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
-            helper.setFrom(getProperties().getUsername());
-            helper.setTo(email);
-            helper.setSubject(title);
-            helper.setText(content, html);
-
-            if (files != null) {
-                for (Map.Entry<String, InputStreamSource> entry : files.entrySet()) {
-                    helper.addAttachment(entry.getKey(), entry.getValue());
+                MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+                helper.setFrom(getProperties().getUsername());
+                helper.setTo(toEmails.toArray(new String[0]));
+                helper.setCc(ccEmails.toArray(new String[0]));
+                helper.setBcc(bccEmails.toArray(new String[0]));
+                if(StringUtils.isNotEmpty(replyEmail)) {
+                    helper.setReplyTo(replyEmail);
                 }
-            }
 
-            getMailSender().send(mimeMessage);
-            return true;
-        } catch (MessagingException | MailException e) {
-            logger.error("发送 带附件的邮件 失败", e);
+                helper.setSubject(subject);
+                helper.setText(content, html);
+
+//                helper.setFileTypeMap(new FileTypeMap());
+                helper.setValidateAddresses(true);
+                helper.setPriority(priority.value);
+                if(sendDate != null) {
+                    helper.setSentDate(sendDate);
+                }
+
+                if(MapUtils.isNotEmpty(inlineFile)) {
+                    for (Map.Entry<String, Resource> entry : inlineFile.entrySet()) {
+                        helper.addInline(entry.getKey(), entry.getValue());
+                    }
+                }
+
+                if (MapUtils.isNotEmpty(extendFile)) {
+                    for (Map.Entry<String, Resource> entry : extendFile.entrySet()) {
+                        helper.addAttachment(entry.getKey(), entry.getValue());
+                    }
+                }
+
+                getMailSender().send(mimeMessage);
+            } catch (MessagingException | MailException e) {
+                logger.error("发送 带附件的邮件 失败", e);
+                throw new MailSendException("发送邮件失败", e);
+            }
         }
-        return false;
+
+        // ====================== Getter And Setter ======================
+        public Builder setSubject(String subject) {
+            this.subject = subject;
+            return this;
+        }
+
+        public Builder setContent(String content) {
+            this.content = content;
+            return this;
+        }
+
+        public Builder setContent(String content, boolean html) {
+            this.content = content;
+            this.html = html;
+            return this;
+        }
+
+        public Builder setReplyEmail(String replyEmail) {
+            this.replyEmail = replyEmail;
+            return this;
+        }
+
+        public Builder setToEmails(Collection<String> toEmails) {
+            this.toEmails = new HashSet<>(ObjectUtils.defaultIfNull(toEmails, new HashSet<>(0)));
+            return this;
+        }
+
+        public Builder addToEmail(String toEmail) {
+            this.toEmails.add(toEmail);
+            return this;
+        }
+
+        public Builder addToEmails(Collection<String> toEmails) {
+            this.toEmails.addAll(toEmails);
+            return this;
+        }
+
+        public Builder setCcEmails(Collection<String> ccEmails) {
+            this.ccEmails = new HashSet<>(ObjectUtils.defaultIfNull(ccEmails, new HashSet<>(0)));
+            return this;
+        }
+
+        public Builder addCcEmail(String ccEmail) {
+            this.ccEmails.add(ccEmail);
+            return this;
+        }
+
+        public Builder addCcEmails(Collection<String> ccEmails) {
+            this.ccEmails.addAll(ccEmails);
+            return this;
+        }
+
+        public Builder setBccEmails(Set<String> bccEmails) {
+            this.bccEmails = new HashSet<>(ObjectUtils.defaultIfNull(bccEmails, new HashSet<>(0)));
+            return this;
+        }
+
+        public Builder addBccEmail(String bccEmail) {
+            this.bccEmails.add(bccEmail);
+            return this;
+        }
+
+        public Builder addBccEmails(Collection<String> bccEmails) {
+            this.bccEmails.addAll(bccEmails);
+            return this;
+        }
+
+        public Builder setInlineFile(Map<String, Resource> inlineFile) {
+            this.inlineFile = new HashMap<>(ObjectUtils.defaultIfNull(inlineFile, new HashMap<>(0)));
+            return this;
+        }
+
+        public Builder addInlineFile(String contentId, Resource resource) {
+            this.inlineFile.put(contentId, resource);
+            return this;
+        }
+
+        public Builder addInlineFile(Map<String, Resource> inlineFile) {
+            this.inlineFile.putAll(inlineFile);
+            return this;
+        }
+
+        public Builder setExtendFile(Map<String, Resource> extendFile) {
+            this.extendFile = new HashMap<>(ObjectUtils.defaultIfNull(extendFile, new HashMap<>(0)));
+            return this;
+        }
+
+        public Builder addExtendFile(String contentId, Resource resource) {
+            this.extendFile.put(contentId, resource);
+            return this;
+        }
+
+        public Builder addExtendFile(Map<String, Resource> extendFile) {
+            this.extendFile.putAll(ObjectUtils.defaultIfNull(extendFile, new HashMap<>(0)));
+            return this;
+        }
+
+        public Builder setPriority(MailPriority priority) {
+            this.priority = ObjectUtils.defaultIfNull(priority, MailPriority.NORMAL);
+            return this;
+        }
+
+        public Builder setSendDate(Date sendDate) {
+            this.sendDate = sendDate;
+            return this;
+        }
+    }
+
+    public enum MailPriority {
+        HIGHEST(1), HIGH(2), NORMAL(3), LOW(4), LOWEST(5);
+        private int value;
+        MailPriority(int value) {
+            this.value = value;
+        }
     }
 }
