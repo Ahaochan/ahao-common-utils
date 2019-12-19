ec3264ef6e725256b0bbb083de7ea86869ba9f8c
Ahaochan <844394093@qq.com>
2019-12-19 17:35:02 +0800

    add AppUtilsTest

---
+++ src/test/java/com/ahao/util/spring/AppUtilsTest.java
@@ -0,0 +1,25 @@
+package com.ahao.util.spring;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@ContextConfiguration(classes = SpringContextHolder.class)
+public class AppUtilsTest {
+
+    @Test
+    public void test() {
+        Environment env = AppUtils.getEnv();
+        Assertions.assertNotNull(env);
+
+        Assertions.assertFalse(AppUtils.isDev());
+        Assertions.assertFalse(AppUtils.isProd());
+        Assertions.assertFalse(AppUtils.isTest());
+    }
+}
