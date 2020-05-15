dfe9917ed0bfa1d9b7c2a6c976a8f2da0fccf2a5
Ahaochan <844394093@qq.com>
2020-05-15 11:11:06 +0800

    rabbit 移动到 common utils

---
+++ src/main/java/com/ahao/mq/rabbit/processor/RabbitBeanPostProcessor.java
@@ -0,0 +1,45 @@
+package com.ahao.mq.rabbit.processor;
+
+import org.springframework.amqp.rabbit.config.AbstractRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+/**
+ * Rabbit 相关Bean的后处理器
+ */
+public class RabbitBeanPostProcessor implements BeanPostProcessor {
+
+    private MessageProcessorCollector messageProcessorCollector;
+    public RabbitBeanPostProcessor() {
+        this(new MessageProcessorCollector());
+    }
+    public RabbitBeanPostProcessor(MessageProcessorCollector messageProcessorCollector) {
+        this.messageProcessorCollector = messageProcessorCollector;
+    }
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        if(bean instanceof RabbitTemplate) {
+            warpRabbitTemplate((RabbitTemplate) bean, beanName);
+        }
+        if (bean instanceof AbstractRabbitListenerContainerFactory) {
+            warpContainerFactory((AbstractRabbitListenerContainerFactory) bean, beanName);
+        }
+        return bean;
+    }
+
+    private void warpContainerFactory(AbstractRabbitListenerContainerFactory bean, String beanName) {
+        bean.setBeforeSendReplyPostProcessors(messageProcessorCollector.getFactoryAfterMessagePostProcessorArray());
+        bean.setAfterReceivePostProcessors(messageProcessorCollector.getFactoryAfterMessagePostProcessorArray()); // 处理 @RabbitListener
+    }
+
+    private void warpRabbitTemplate(RabbitTemplate bean, String beanName) {
+        bean.setBeforePublishPostProcessors(messageProcessorCollector.getTemplateBeforeMessagePostProcessorArray());
+        bean.setAfterReceivePostProcessors(messageProcessorCollector.getTemplateAfterMessagePostProcessorArray());
+
+        // bean.setConfirmCallback();
+        // bean.setReturnCallback();
+        // bean.setRecoveryCallback();
+    }
+}
