91ed9c75268712755b5ff5c79b6464342304bd9c
Ahaochan <844394093@qq.com>
2020-05-15 11:42:27 +0800

    rabbit 支持 透传

--- src/main/java/com/ahao/mq/rabbit/RabbitAutoConfig.java
+++ src/main/java/com/ahao/mq/rabbit/RabbitAutoConfig.java
@@ -3,13 +3,16 @@ package com.ahao.mq.rabbit;
 import com.ahao.mq.rabbit.convert.JsonMessageConverter;
 import com.ahao.mq.rabbit.processor.MessageProcessorCollector;
 import com.ahao.mq.rabbit.processor.RabbitBeanPostProcessor;
+import com.ahao.transmit.interceptor.TransmitRabbitMessagePostProcessor;
 import com.ahao.util.spring.mq.RabbitMQHelper;
 import com.rabbitmq.client.Channel;
 import org.springframework.amqp.core.CustomExchange;
 import org.springframework.amqp.core.Exchange;
+import org.springframework.amqp.core.MessagePostProcessor;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.amqp.RabbitProperties;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -19,7 +22,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Configuration(proxyBeanMethods = false)
@@ -48,9 +53,20 @@ public class RabbitAutoConfig {
 
     @Bean
     @ConditionalOnMissingBean
-    public MessageProcessorCollector messageProcessorCollector() {
-        MessageProcessorCollector messageProcessorCollector = new MessageProcessorCollector();
-        return messageProcessorCollector;
+    public MessageProcessorCollector messageProcessorCollector(ObjectProvider<TransmitRabbitMessagePostProcessor.Before> transmitBefore,
+                                                               ObjectProvider<TransmitRabbitMessagePostProcessor.After> transmitAfter) {
+        List<MessagePostProcessor> beforeList = new ArrayList<>();
+        transmitBefore.ifAvailable(beforeList::add);
+
+        List<MessagePostProcessor> afterList = new ArrayList<>();
+        transmitAfter.ifAvailable(afterList::add);
+
+        MessageProcessorCollector collector = new MessageProcessorCollector();
+        collector.setTemplateBeforeMessagePostProcessorList(beforeList);
+        collector.setTemplateAfterMessagePostProcessorList(afterList);
+        collector.setFactoryBeforeMessagePostProcessorList(beforeList);
+        collector.setFactoryAfterMessagePostProcessorList(afterList);
+        return collector;
     }
 
     @Bean
