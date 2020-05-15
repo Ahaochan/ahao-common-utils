6b8600f9b383b21e3499b8fbc1169b1ec30a933c
Ahaochan <844394093@qq.com>
2020-05-15 10:58:40 +0800

    hystrix 移动到 common utils

---
+++ src/main/java/com/ahao/hystrix/HystrixAutoConfig.java
@@ -0,0 +1,15 @@
+package com.ahao.hystrix;
+
+import com.netflix.hystrix.HystrixCommand;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration(proxyBeanMethods = false)
+@ConditionalOnClass(HystrixCommand.class)
+public class HystrixAutoConfig {
+    @Bean
+    public TransmitHystrixConcurrencyStrategy transmitHystrixConcurrencyStrategy() {
+        return new TransmitHystrixConcurrencyStrategy();
+    }
+}
