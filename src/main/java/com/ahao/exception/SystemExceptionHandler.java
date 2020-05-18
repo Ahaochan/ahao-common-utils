5ade874baae524761e16725fb68b6f9edad9c97a
Ahaochan <844394093@qq.com>
2020-05-18 19:50:46 +0800

    异常处理优化

---
+++ src/main/java/com/ahao/exception/SystemExceptionHandler.java
@@ -0,0 +1,45 @@
+package com.ahao.exception;
+
+import com.ahao.domain.entity.AjaxDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 系统异常拦截器
+ */
+@RestControllerAdvice("com.ahao")
+@Order(Ordered.LOWEST_PRECEDENCE)
+public class SystemExceptionHandler {
+    public static final Logger logger = LoggerFactory.getLogger(SystemExceptionHandler.class);
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    public AjaxDTO methodNotAllowed(HttpServletRequest request, HttpRequestMethodNotSupportedException e) {
+        String message = String.format("该请求不支持%s方法, 请使用%s方法", e.getMethod(), e.getSupportedHttpMethods());
+        logger.debug(message);
+        return AjaxDTO.failure(message);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public AjaxDTO runtimeException(RuntimeException e) {
+        logger.error("运行时异常:", e);
+        return AjaxDTO.failure("服务器异常! 请稍候重试!");
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public AjaxDTO runtimeException(Exception e) {
+        logger.error("异常:", e);
+        return AjaxDTO.failure("服务器异常! 请稍候重试!");
+    }
+}
---
+++ src/main/java/com/ahao/exception/ValidatorExceptionHandler.java
@@ -0,0 +1,75 @@
+package com.ahao.exception;
+
+import com.ahao.domain.entity.AjaxDTO;
+import org.apache.commons.lang3.ObjectUtils;
+import org.springframework.core.annotation.Order;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * Spring校验异常拦截器
+ */
+@RestControllerAdvice("com.ahao")
+@Order(0)
+public class ValidatorExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public AjaxDTO methodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        // 1. 找到 BindingResult 参数
+        BindingResult bindingResult = ex.getBindingResult();
+        List<ObjectError> errors = bindingResult.getAllErrors();
+
+        // 2. 生成错误信息
+        Map<String, String> fieldErrors = errors.stream()
+            .filter(e -> e instanceof FieldError)
+            .map(e -> (FieldError) e)
+            .collect(Collectors.toMap(FieldError::getField, e -> ObjectUtils.defaultIfNull(e.getDefaultMessage(), "")));
+
+        String firstErrorMsg = errors.get(0).getDefaultMessage();
+        return AjaxDTO.failure(firstErrorMsg, fieldErrors);
+    }
+
+    @ExceptionHandler(BindException.class)
+    public AjaxDTO bindException(BindException ex) {
+        // 1. 找到 BindingResult 参数
+        BindingResult bindingResult = ex.getBindingResult();
+        List<ObjectError> errors = bindingResult.getAllErrors();
+
+        // 2. 生成错误信息
+        Map<String, String> fieldErrors = errors.stream()
+            .filter(e -> e instanceof FieldError)
+            .map(e -> (FieldError) e)
+            .collect(Collectors.toMap(FieldError::getField, e -> ObjectUtils.defaultIfNull(e.getDefaultMessage(), "")));
+
+        String firstErrorMsg = errors.get(0).getDefaultMessage();
+        return AjaxDTO.failure(firstErrorMsg, fieldErrors);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public AjaxDTO constraintViolationException(ConstraintViolationException ex) {
+        // 1. 生成错误信息
+        Map<String, String> fieldErrors = new HashMap<>();
+        Set<ConstraintViolation<?>> constraintViolations = ex.getConstraintViolations();
+        for (ConstraintViolation<?> constraintViolation : constraintViolations) {
+            String path = String.valueOf(constraintViolation.getPropertyPath());
+            String field = path.substring(Math.max(path.indexOf('.') + 1, 0)); // 排除第一个节点
+            String message = constraintViolation.getMessage();
+            fieldErrors.put(field, message);
+        }
+
+        String firstErrorMsg = fieldErrors.values().iterator().next();
+        return AjaxDTO.failure(firstErrorMsg, fieldErrors);
+    }
+}
