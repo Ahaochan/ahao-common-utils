09c152d727bad5838707b3b2383817807808120a
Ahaochan <844394093@qq.com>
2020-05-19 14:41:07 +0800

    add ConditionalOnClass

--- src/main/java/com/ahao/exception/MyBatisExceptionHandler.java
+++ src/main/java/com/ahao/exception/MyBatisExceptionHandler.java
@@ -4,6 +4,7 @@ import com.ahao.domain.entity.AjaxDTO;
 import org.mybatis.spring.MyBatisSystemException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.core.annotation.Order;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,6 +16,7 @@ import javax.persistence.PersistenceException;
  */
 @RestControllerAdvice("com.ahao")
 @Order(0)
+@ConditionalOnClass({MyBatisSystemException.class, PersistenceException.class})
 public class MyBatisExceptionHandler {
     public static final Logger logger = LoggerFactory.getLogger(MyBatisExceptionHandler.class);
 
@@ -30,7 +32,7 @@ public class MyBatisExceptionHandler {
     public AjaxDTO myBatisSystemException(MyBatisSystemException ex) {
         logger.error("Mybatis 错误", ex);
         Throwable cause = ex.getCause(); // 获取 MyBatisSystemException 的 cause
-        if(cause != null && cause.getCause() != null) {
+        if (cause != null && cause.getCause() != null) {
             cause = cause.getCause(); // 获取 PersistenceException 的 cause
         }
         String errorMsg = cause == null ? ex.getMessage() : cause.getMessage();
