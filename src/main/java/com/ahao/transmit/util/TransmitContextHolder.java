75422973312811416daa709820c759ee8670e2bb
Ahaochan <844394093@qq.com>
2020-09-09 22:01:56 +0800

    动态支持透传参数

--- src/main/java/com/ahao/transmit/util/TransmitContextHolder.java
+++ src/main/java/com/ahao/transmit/util/TransmitContextHolder.java
@@ -4,32 +4,33 @@ import com.alibaba.ttl.TransmittableThreadLocal;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class TransmitContextHolder {
     public static final Logger logger = LoggerFactory.getLogger(TransmitContextHolder.class);
     public static final String HEADER_TOKEN = "token";
 
-    private static ThreadLocal<String> token = new TransmittableThreadLocal<>();
-    private static ThreadLocal<String> tenant = new TransmittableThreadLocal<>();
+    private static ThreadLocal<Map<String, String>> holder = new TransmittableThreadLocal<Map<String, String>>() {
+        @Override
+        protected Map<String, String> initialValue() {
+            return new ConcurrentHashMap<>();
+        }
+    };
 
     public static void set(String key, String value) {
-        switch (key) {
-            case HEADER_TOKEN: token.set(value); break;
-            default: logger.debug("不支持透传header:{}" + key);
-        }
+        holder.get().put(key, value);
     }
 
     public static String get(String key) {
-        switch (key) {
-            case HEADER_TOKEN: return token.get();
-            default: logger.debug("不支持透传header:{}" + key);
-        }
-        return null;
+        return holder.get().get(key);
     }
 
-    public static void remove(String key) {
-        switch (key) {
-            case HEADER_TOKEN: token.remove(); break;
-            default: logger.debug("不支持透传header:{}" + key);
-        }
+    public static String getOrDefault(String key, String defaultValue) {
+        return holder.get().getOrDefault(key, defaultValue);
+    }
+
+    public static String remove(String key) {
+        return holder.get().remove(key);
     }
 }
