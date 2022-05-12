1a45bd037d5098a2696041787b052ee71f252580
Ahaochan <844394093@qq.com>
2022-05-12 09:28:51 +0800

    ✨ 对已有Bean进行动态代理

--- src/test/java/moe/ahao/spring/SpringBeanTest.java
+++ src/test/java/moe/ahao/spring/SpringBeanTest.java
@@ -2,10 +2,16 @@ package moe.ahao.spring;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.UnsatisfiedDependencyException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.lang.reflect.Proxy;
+
 class SpringBeanTest {
+    interface AhaoInterface {}
+    static class AhaoInterfaceImpl implements AhaoInterface {}
     static class AhaoBean {}
     class NoSuchBean {}
     @Test
@@ -21,5 +27,32 @@ class SpringBeanTest {
         context.registerBean(NoSuchBean.class);
         // context.refresh();
         Assertions.assertThrows(UnsatisfiedDependencyException.class, context::refresh);
+    }
+    @Test
+    void proxy() {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.registerBean(AhaoInterfaceImpl.class);
+        context.registerBean(AhaoBeanPostProcessor.class);
+        context.refresh();
+
+        AhaoInterface bean = context.getBean(AhaoInterface.class);
+        Assertions.assertNotNull(bean);
+        System.out.println("替换后的类名:" + bean.getClass().getName());
+        Assertions.assertNotEquals(AhaoInterfaceImpl.class, bean.getClass());
+        System.out.println(context.getBean(AhaoInterface.class).toString());
+    }
+
+
+    public static class AhaoBeanPostProcessor implements BeanPostProcessor {
+        @Override
+        public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+            Class<?> clazz = bean.getClass();
+            System.out.println("对已有Bean进行动态代理:" + clazz.getName());
+            return Proxy.newProxyInstance(clazz.getClassLoader(), clazz.getInterfaces(), (p, m, a) -> {
+                System.out.print("JDK动态代理:");
+                // return method.invoke(proxy, args); // loop
+                return m.invoke(bean, a);
+            });
+        }
     }
 }
