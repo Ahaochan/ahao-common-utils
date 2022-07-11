6dd24e9cdef8c80debba01826f66e5b293235d46
Ahaochan <844394093@qq.com>
2022-07-11 13:02:16 +0800

    ✅ CyclicBarrier使用

---
+++ src/test/java/moe/ahao/util/commons/juc/CyclicBarrierTest.java
@@ -0,0 +1,31 @@
+package moe.ahao.util.commons.juc;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.CyclicBarrier;
+
+class CyclicBarrierTest {
+    @Test
+    void test() {
+        int parties = 5;
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(parties, new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("全部抵达栅栏");
+            }
+        });
+
+        for (int i = 0; i < parties; i++) {
+            int index = i;
+            new Thread(() -> {
+                try {
+                    System.out.println("到达栅栏" + index);
+                    cyclicBarrier.await();
+                    System.out.println("冲破栅栏" + index);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        }
+    }
+}
