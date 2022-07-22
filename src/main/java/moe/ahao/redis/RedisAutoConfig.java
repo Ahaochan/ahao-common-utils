93eae306356091600fcae58bd31aaa41ef0c39e2
Ahaochan <844394093@qq.com>
2022-07-22 20:03:31 +0800

    🚚 Redis自定义配置到ahao-common-utils

---
+++ src/main/java/moe/ahao/redis/RedisAutoConfig.java
@@ -0,0 +1,32 @@
+package moe.ahao.redis;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+@Configuration(proxyBeanMethods = false)
+@ConditionalOnClass(RedisTemplate.class)
+public class RedisAutoConfig {
+
+    @Bean(name = "redisTemplate")
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setKeySerializer(RedisSerializer.string());
+        template.setValueSerializer(RedisSerializer.json());
+        template.setConnectionFactory(redisConnectionFactory);
+        return template;
+    }
+
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        StringRedisTemplate template = new StringRedisTemplate();
+        template.setKeySerializer(RedisSerializer.string());
+        template.setValueSerializer(RedisSerializer.string());
+        template.setConnectionFactory(redisConnectionFactory);
+        return template;
+    }
+}
