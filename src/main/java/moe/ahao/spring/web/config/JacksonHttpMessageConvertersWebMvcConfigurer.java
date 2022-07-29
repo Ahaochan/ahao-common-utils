12863df178d09cc8f0faf729ff55df2084b65a59
Ahaochan <844394093@qq.com>
2022-07-29 17:20:44 +0800

    🐛 修复spring.jackson.date-format失效的问题

--- src/main/java/moe/ahao/spring/web/config/JacksonHttpMessageConvertersWebMvcConfigurer.java
+++ src/main/java/moe/ahao/spring/web/config/JacksonHttpMessageConvertersWebMvcConfigurer.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
-import org.springframework.boot.autoconfigure.http.JacksonHttpMessageConvertersConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
