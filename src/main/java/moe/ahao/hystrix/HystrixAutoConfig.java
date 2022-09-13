9600b2020730ac1450b6b773aecb0142754c3c6a
Ahaochan <844394093@qq.com>
2022-09-13 16:43:32 +0800

    🐛 修复spring cloud gateway找不到Filter的bug

--- src/main/java/moe/ahao/hystrix/HystrixAutoConfig.java
+++ src/main/java/moe/ahao/hystrix/HystrixAutoConfig.java
@@ -1,10 +1,8 @@
 package moe.ahao.hystrix;
 
 import com.netflix.hystrix.HystrixCommand;
-import moe.ahao.transmit.filter.TransmitFilter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
-import org.springframework.boot.autoconfigure.web.servlet.ConditionalOnMissingFilterBean;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,7 +16,6 @@ public class HystrixAutoConfig {
     }
 
     @Bean
-    @ConditionalOnMissingFilterBean(TransmitFilter.class)
     @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
     public FilterRegistrationBean<HystrixCacheFilter> hystrixCacheFilterFilterRegistrationBean() {
         FilterRegistrationBean<HystrixCacheFilter> bean = HystrixCacheFilter.buildFilterBean("/*");
