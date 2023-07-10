e2dd9013e2d72a61ed10dc1ce00a533d3851be41
Ahaochan <844394093@qq.com>
2023-07-10 12:50:00 +0800

    ✨ Spring @EventListener执行顺序

--- src/test/java/moe/ahao/util/spring/EventTest.java
+++ src/test/java/moe/ahao/util/spring/EventTest.java
@@ -1,10 +1,13 @@
 package moe.ahao.util.spring;
 
+import org.apache.commons.lang3.time.DateFormatUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.event.EventListener;
+
+import java.util.Date;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE,
     classes = {SpringContextHolder.class, EventTest.class})
@@ -19,7 +22,20 @@ class EventTest {
     }
 
     @EventListener
-    public void listener(String msg) {
-        System.out.println(msg);
+    public void listener1(String msg1) throws Exception {
+        System.out.println("msg1: " + msg1 + "现在时间: " + DateFormatUtils.format(new Date(), "HH:mm:ss.SSS"));
+        Thread.sleep(1000);
+    }
+
+    @EventListener
+    public void listener2(String msg2) throws Exception {
+        System.out.println("msg2: " + msg2 + "现在时间: " + DateFormatUtils.format(new Date(), "HH:mm:ss.SSS"));
+        Thread.sleep(1000);
+    }
+
+    @EventListener
+    public void listener3(String msg3) throws Exception {
+        System.out.println("msg3: " + msg3 + "现在时间: " + DateFormatUtils.format(new Date(), "HH:mm:ss.SSS"));
+        Thread.sleep(1000);
     }
 }
