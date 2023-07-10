cfbc85711e5d38ec2c0960e81fb5af0eef7aac27
Ahaochan <844394093@qq.com>
2023-07-10 12:54:12 +0800

    ✨ Spring @EventListener执行顺序

--- src/test/java/moe/ahao/util/spring/EventTest.java
+++ src/test/java/moe/ahao/util/spring/EventTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.annotation.Order;
 
 import java.util.Date;
 
@@ -22,18 +23,21 @@ class EventTest {
     }
 
     @EventListener
+    @Order(1)
     public void listener1(String msg1) throws Exception {
         System.out.println("msg1: " + msg1 + "现在时间: " + DateFormatUtils.format(new Date(), "HH:mm:ss.SSS"));
         Thread.sleep(1000);
     }
 
     @EventListener
+    @Order(2)
     public void listener2(String msg2) throws Exception {
         System.out.println("msg2: " + msg2 + "现在时间: " + DateFormatUtils.format(new Date(), "HH:mm:ss.SSS"));
         Thread.sleep(1000);
     }
 
     @EventListener
+    @Order(3)
     public void listener3(String msg3) throws Exception {
         System.out.println("msg3: " + msg3 + "现在时间: " + DateFormatUtils.format(new Date(), "HH:mm:ss.SSS"));
         Thread.sleep(1000);
